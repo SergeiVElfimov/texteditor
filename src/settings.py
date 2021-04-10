c0fa7a8d4199cd08eb46a5d219c4e02953134095
Sergey V. Elfimov <elfimovserg@gmail.com>
2021-04-11 01:40:26 +0300

    change settings and req

---
+++ src/settings.py
@@ -0,0 +1,3 @@
+FONT_SIZES = [7, 8, 9, 10, 11, 12, 13, 14, 18, 24, 36, 48, 64, 72, 96, 144, 288]
+IMAGE_EXTENSIONS = [".jpg", ".png", ".bmp"]
+TEXT_EXTENSIONS = [".txt"]
--- src/wordprocessor.py
+++ src/wordprocessor.py
@@ -1,7 +1,7 @@
 import os
 
 from PyQt6.QtCore import QSize, Qt
-from PyQt6.QtGui import QAction, QActionGroup, QFont, QIcon, QImage, QKeySequence, QTextDocument
+from PyQt6.QtGui import QAction, QActionGroup, QFont, QIcon, QKeySequence
 from PyQt6.QtPrintSupport import QPrintDialog
 from PyQt6.QtWidgets import (
     QComboBox,
@@ -16,48 +16,9 @@ from PyQt6.QtWidgets import (
     QWidget,
 )
 
-from .utils import hexuuid, splitext
-
-FONT_SIZES = [7, 8, 9, 10, 11, 12, 13, 14, 18, 24, 36, 48, 64, 72, 96, 144, 288]
-IMAGE_EXTENSIONS = [".jpg", ".png", ".bmp"]
-TEXT_EXTENSIONS = [".txt"]
-
-
-class TextEdit(QTextEdit):
-    def canInsertFromMimeData(self, source):
-
-        if source.hasImage():
-            return True
-        else:
-            return super(TextEdit, self).canInsertFromMimeData(source)
-
-    def insertFromMimeData(self, source):
-
-        cursor = self.textCursor()
-        document = self.document()
-
-        if source.hasUrls():
-
-            for u in source.urls():
-                file_ext = splitext(str(u.toLocalFile()))
-                if u.isLocalFile() and file_ext in IMAGE_EXTENSIONS:
-                    image = QImage(u.toLocalFile())
-                    document.addResource(QTextDocument.ImageResource, u, image)
-                    cursor.insertImage(u.toLocalFile())
-                else:
-                    break
-
-            else:
-                return
-
-        elif source.hasImage():
-            image = source.imageData()
-            uuid = hexuuid()
-            document.addResource(QTextDocument.ImageResource, uuid, image)
-            cursor.insertImage(uuid)
-            return
-
-        super(TextEdit, self).insertFromMimeData(source)
+from .settings import FONT_SIZES, TEXT_EXTENSIONS
+from .textedit import TextEdit
+from .utils import splitext
 
 
 class MainWindow(QMainWindow):
@@ -98,25 +59,19 @@ class MainWindow(QMainWindow):
         file_menu.addAction(open_file_action)
         file_toolbar.addAction(open_file_action)
 
-        save_file_action = QAction(
-            QIcon(os.path.join("images", "disk.png")), "Save", self
-        )
+        save_file_action = QAction(QIcon(os.path.join("images", "disk.png")), "Save", self)
         save_file_action.setStatusTip("Save current page")
         save_file_action.triggered.connect(self.file_save)
         file_menu.addAction(save_file_action)
         file_toolbar.addAction(save_file_action)
 
-        save_as_file_action = QAction(
-            QIcon(os.path.join("images", "disk--pencil.png")), "Save As...", self
-        )
+        save_as_file_action = QAction(QIcon(os.path.join("images", "disk--pencil.png")), "Save As...", self)
         save_as_file_action.setStatusTip("Save current page to specified file")
         save_as_file_action.triggered.connect(self.file_save_as)
         file_menu.addAction(save_as_file_action)
         file_toolbar.addAction(save_as_file_action)
 
-        print_action = QAction(
-            QIcon(os.path.join("images", "printer.png")), "Print...", self
-        )
+        print_action = QAction(QIcon(os.path.join("images", "printer.png")), "Print...", self)
         print_action.setStatusTip("Print current page")
         print_action.triggered.connect(self.file_print)
         file_menu.addAction(print_action)
@@ -127,16 +82,12 @@ class MainWindow(QMainWindow):
         self.addToolBar(edit_toolbar)
         edit_menu = self.menuBar().addMenu("&Edit")
 
-        undo_action = QAction(
-            QIcon(os.path.join("images", "arrow-curve-180-left.png")), "Undo", self
-        )
+        undo_action = QAction(QIcon(os.path.join("images", "arrow-curve-180-left.png")), "Undo", self)
         undo_action.setStatusTip("Undo last change")
         undo_action.triggered.connect(self.editor.undo)
         edit_menu.addAction(undo_action)
 
-        redo_action = QAction(
-            QIcon(os.path.join("images", "arrow-curve.png")), "Redo", self
-        )
+        redo_action = QAction(QIcon(os.path.join("images", "arrow-curve.png")), "Redo", self)
         redo_action.setStatusTip("Redo last change")
         redo_action.triggered.connect(self.editor.redo)
         edit_toolbar.addAction(redo_action)
@@ -151,9 +102,7 @@ class MainWindow(QMainWindow):
         edit_toolbar.addAction(cut_action)
         edit_menu.addAction(cut_action)
 
-        copy_action = QAction(
-            QIcon(os.path.join("images", "document-copy.png")), "Copy", self
-        )
+        copy_action = QAction(QIcon(os.path.join("images", "document-copy.png")), "Copy", self)
         copy_action.setStatusTip("Copy selected text")
         cut_action.setShortcut(QKeySequence.StandardKey.Copy)
         copy_action.triggered.connect(self.editor.copy)
@@ -171,9 +120,7 @@ class MainWindow(QMainWindow):
         edit_toolbar.addAction(paste_action)
         edit_menu.addAction(paste_action)
 
-        select_action = QAction(
-            QIcon(os.path.join("images", "selection-input.png")), "Select all", self
-        )
+        select_action = QAction(QIcon(os.path.join("images", "selection-input.png")), "Select all", self)
         select_action.setStatusTip("Select all text")
         cut_action.setShortcut(QKeySequence.StandardKey.SelectAll)
         select_action.triggered.connect(self.editor.selectAll)
@@ -204,28 +151,20 @@ class MainWindow(QMainWindow):
         self.fontsize = QComboBox()
         self.fontsize.addItems([str(s) for s in FONT_SIZES])
 
-        self.fontsize.currentTextChanged[str].connect(
-            lambda s: self.editor.setFontPointSize(float(s))
-        )
+        self.fontsize.currentTextChanged[str].connect(lambda s: self.editor.setFontPointSize(float(s)))
         format_toolbar.addWidget(self.fontsize)
 
-        self.bold_action = QAction(
-            QIcon(os.path.join("images", "edit-bold.png")), "Bold", self
-        )
+        self.bold_action = QAction(QIcon(os.path.join("images", "edit-bold.png")), "Bold", self)
         self.bold_action.setStatusTip("Bold")
         self.bold_action.setShortcut(QKeySequence.StandardKey.Bold)
         self.bold_action.setCheckable(True)
         self.bold_action.toggled.connect(
-            lambda x: self.editor.setFontWeight(
-                QFont.Weight.Bold if x else QFont.Weight.Normal
-            )
+            lambda x: self.editor.setFontWeight(QFont.Weight.Bold if x else QFont.Weight.Normal)
         )
         format_toolbar.addAction(self.bold_action)
         format_menu.addAction(self.bold_action)
 
-        self.italic_action = QAction(
-            QIcon(os.path.join("images", "edit-italic.png")), "Italic", self
-        )
+        self.italic_action = QAction(QIcon(os.path.join("images", "edit-italic.png")), "Italic", self)
         self.italic_action.setStatusTip("Italic")
         self.italic_action.setShortcut(QKeySequence.StandardKey.Italic)
         self.italic_action.setCheckable(True)
@@ -233,9 +172,7 @@ class MainWindow(QMainWindow):
         format_toolbar.addAction(self.italic_action)
         format_menu.addAction(self.italic_action)
 
-        self.underline_action = QAction(
-            QIcon(os.path.join("images", "edit-underline.png")), "Underline", self
-        )
+        self.underline_action = QAction(QIcon(os.path.join("images", "edit-underline.png")), "Underline", self)
         self.underline_action.setStatusTip("Underline")
         self.underline_action.setShortcut(QKeySequence.StandardKey.Underline)
         self.underline_action.setCheckable(True)
@@ -245,14 +182,10 @@ class MainWindow(QMainWindow):
 
         format_menu.addSeparator()
 
-        self.align_left_action = QAction(
-            QIcon(os.path.join("images", "edit-alignment.png")), "Align left", self
-        )
+        self.align_left_action = QAction(QIcon(os.path.join("images", "edit-alignment.png")), "Align left", self)
         self.align_left_action.setStatusTip("Align text left")
         self.align_left_action.setCheckable(True)
-        self.align_left_action.triggered.connect(
-            lambda: self.editor.setAlignment(Qt.Alignment.AlignLeft)
-        )
+        self.align_left_action.triggered.connect(lambda: self.editor.setAlignment(Qt.Alignment.AlignLeft))
         format_toolbar.addAction(self.align_left_action)
         format_menu.addAction(self.align_left_action)
 
@@ -263,9 +196,7 @@ class MainWindow(QMainWindow):
         )
         self.align_center_action.setStatusTip("Align text center")
         self.align_center_action.setCheckable(True)
-        self.align_center_action.triggered.connect(
-            lambda: self.editor.setAlignment(Qt.Alignment.AlignCenter)
-        )
+        self.align_center_action.triggered.connect(lambda: self.editor.setAlignment(Qt.Alignment.AlignCenter))
         format_toolbar.addAction(self.align_center_action)
         format_menu.addAction(self.align_center_action)
 
@@ -276,9 +207,7 @@ class MainWindow(QMainWindow):
         )
         self.align_right_action.setStatusTip("Align text right")
         self.align_right_action.setCheckable(True)
-        self.align_right_action.triggered.connect(
-            lambda: self.editor.setAlignment(Qt.Alignment.AlignRight)
-        )
+        self.align_right_action.triggered.connect(lambda: self.editor.setAlignment(Qt.Alignment.AlignRight))
         format_toolbar.addAction(self.align_right_action)
         format_menu.addAction(self.align_right_action)
 
@@ -287,9 +216,7 @@ class MainWindow(QMainWindow):
         )
         self.align_justify_action.setStatusTip("Justify text")
         self.align_justify_action.setCheckable(True)
-        self.align_justify_action.triggered.connect(
-            lambda: self.editor.setAlignment(Qt.Alignment.AlignJustify)
-        )
+        self.align_justify_action.triggered.connect(lambda: self.editor.setAlignment(Qt.Alignment.AlignJustify))
         format_toolbar.addAction(self.align_justify_action)
         format_menu.addAction(self.align_justify_action)
 
@@ -331,18 +258,10 @@ class MainWindow(QMainWindow):
         self.italic_action.setChecked(self.editor.fontItalic())
         self.underline_action.setChecked(self.editor.fontUnderline())
         self.bold_action.setChecked(self.editor.fontWeight() == QFont.bold)
-        self.align_left_action.setChecked(
-            self.editor.alignment() == Qt.Alignment.AlignLeft
-        )
-        self.align_center_action.setChecked(
-            self.editor.alignment() == Qt.Alignment.AlignCenter
-        )
-        self.align_right_action.setChecked(
-            self.editor.alignment() == Qt.Alignment.AlignRight
-        )
-        self.align_justify_action.setChecked(
-            self.editor.alignment() == Qt.Alignment.AlignJustify
-        )
+        self.align_left_action.setChecked(self.editor.alignment() == Qt.Alignment.AlignLeft)
+        self.align_center_action.setChecked(self.editor.alignment() == Qt.Alignment.AlignCenter)
+        self.align_right_action.setChecked(self.editor.alignment() == Qt.Alignment.AlignRight)
+        self.align_justify_action.setChecked(self.editor.alignment() == Qt.Alignment.AlignJustify)
 
         self.block_signals(self._format_actions, False)
 
@@ -376,11 +295,7 @@ class MainWindow(QMainWindow):
         if self.path is None:
             return self.file_save_as()
 
-        text = (
-            self.editor.toHtml()
-            if splitext(self.path) in TEXT_EXTENSIONS
-            else self.editor.toPlainText()
-        )
+        text = self.editor.toHtml() if splitext(self.path) in TEXT_EXTENSIONS else self.editor.toPlainText()
 
         try:
             with open(self.path, "w") as f:
@@ -400,11 +315,7 @@ class MainWindow(QMainWindow):
         if not path:
             return
 
-        text = (
-            self.editor.toHtml()
-            if splitext(path) in TEXT_EXTENSIONS
-            else self.editor.toPlainText()
-        )
+        text = self.editor.toPlainText() if splitext(path) in TEXT_EXTENSIONS else self.editor.toPlainText()
 
         try:
             with open(path, "w") as f:
@@ -423,10 +334,7 @@ class MainWindow(QMainWindow):
             self.editor.print_(dlg.printer())
 
     def update_title(self):
-        self.setWindowTitle(
-            "%s - Megasolid Idiom"
-            % (os.path.basename(self.path) if self.path else "Untitled")
-        )
+        self.setWindowTitle("%s - Megasolid Idiom" % (os.path.basename(self.path) if self.path else "Untitled"))
 
     def edit_toggle_wrap(self):
         self.editor.setLineWrapMode(1 if self.editor.lineWrapMode() == 0 else 0)
